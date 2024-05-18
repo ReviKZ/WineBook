0349462064cbd8946a4f6bb060ee81d3953bec49
Kovács Zoárd <99755907+ReviKZ@users.noreply.github.com>
2024-05-18 13:34:05 +0200

    Created Project files

---
+++ winebook/src/main/java/com/revikz/winebook/WinebookApplication.java
@@ -0,0 +1,13 @@
+package com.revikz.winebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinebookApplication.class, args);
+	}
+
+}
---
+++ winebook/src/test/java/com/revikz/winebook/WinebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revikz.winebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
