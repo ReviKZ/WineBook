6b478a89973ff71235495ab0dbe8157df2aeadbb
ReviKZ <kovacs.zoard.1@gmail.com>
2024-05-19 14:27:57 +0200

    Edit some variables and queries so it now works with the front end;

--- winebook/src/main/java/com/revikz/winebook/WinebookApplication.java
+++ winebook/src/main/java/com/revikz/winebook/WinebookApplication.java
@@ -1,13 +1,17 @@
 package com.revikz.winebook;
 
-import org.springframework.boot.SpringApplication;
+import com.revikz.winebook.user.UserRegisterLoginView;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WinebookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WinebookApplication.class, args);
+		//SpringApplication.run(WinebookApplication.class, args);
+		var ctx = new SpringApplicationBuilder(WinebookApplication.class).headless(false).run(args);
+
+		ctx.getBean(UserRegisterLoginView.class);
 	}
 
 }
